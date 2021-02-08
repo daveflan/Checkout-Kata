35e55893585754f87325edf6058600cb46c962f9
daveflan <daveflan@gmail.com>
2021-02-08 10:59:57 +0000

    Updated Pricing

---
+++ Checkout Kata/ItemDetail.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Checkout_Kata
+{
+   
+    public class ItemDetail
+    {
+        private Item _item;
+        private int _quantity;
+
+        public ItemDetail(Item item, int quantity)
+        {
+            _item = item;
+            _quantity = quantity;
+        }
+
+        public decimal GetSKUCost()
+        {
+            return _item.GetItemPrice() * _quantity;
+        }
+    }
+
+
+
+    
+}
---
+++ Checkout Kata/ScanItems.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkout_Kata
+{
+    class ScanItems
+    {
+        public ScanItems()
+        {
+            _itemDetails = new List<ItemDetail>();
+        }
+
+        public void Add(ItemDetail itemDetail)
+        {
+            _itemDetails.Add(itemDetail);
+        }
+
+        List<ItemDetail> _itemDetails;
+
+        public decimal GetTotalPrice()
+        {
+            var totalPrice = 0.0M;
+            foreach (var itemDetail in _itemDetails)
+                totalPrice += itemDetail.GetSKUCost();
+
+            return totalPrice;
+        }
+    }
+}
